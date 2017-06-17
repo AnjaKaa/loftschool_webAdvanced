83ac39fc76de9c9ea8141f9827ed74f47ec5583e
VlaSovaAnna <anja_kaa@mail.ru>
2017-06-17 15:09:10 +0300

    учебная сбока

---
+++ loftschool_webAdvanced/gulp/tasks/sprite.png_gif.js
@@ -0,0 +1,20 @@
+ 'use strict';
+
+module.exports = function() {
+	var sprite = require('gulp.spritesmith');
+
+    $.gulp.task('spriteOutput', function() {
+         var spriteOutput = $.gulp.src("./source/sprite/*.+(png|gif)")
+         .pipe(sprite ( { 
+         	imgName: 'sprite.png', 
+         	cssName: 'sprite.css', 
+         	})
+         );
+ 
+        spriteOutput.css.pipe($.gulp.dest($.config.root + '/assets/css'));
+        spriteOutput.img.pipe($.gulp.dest($.config.root + '/assets/img'));
+        return  spriteOutput;
+    });
+ };
+
+
